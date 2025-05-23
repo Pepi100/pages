fcde788cd28ef068b10566a702547307f178243c
Pepi100 <radunedelcu02@gmail.com>
2025-05-23 19:56:46 +0300

    metadata + new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist,} from "next/font/google";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Korean Numbers Practice",
-  description: "TODO",
+  title: "Korean Numbers Practice - Sino & Native Number Trainer",
+  description:
+    "Master Korean numbers effortlessly with this interactive tool. Practice both Sino-Korean and Native Korean numbers with instant feedback and adjustable ranges.",
 };
 
 export default function RootLayout({
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={geistSans.className}
       >
         {children}
       </body>
